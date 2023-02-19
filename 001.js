cc802bda15fdf124bd4647755637ee5c7a478bcc
Jacorb91 <124224543+Jacorb91@users.noreply.github.com>
2023-02-19 16:00:55 -0300

    Create 001.js

---
+++ 001.js
@@ -0,0 +1,3 @@
+document.cookie="gender=male; expires=Jan 1 2024; path=/2023/02/game001.html"
+document.cookie="powered1=AddThis; expires=Jan 1 2024; path=/2023/02/game001.html"
+document.cookie="madewith=Blogger; expires=Jan 1 2024; path=/2023/02/game001.html"
